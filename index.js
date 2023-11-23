393121aa536132a7add5c692ef6780cc8cebdc67
lukas <lukkotecki@gmail.com>
2023-11-23 16:01:37 +0100

    first layout finished

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 import { menuArray } from "/data.js"
 
 let orderList = [];
-orderList = menuArray
+orderList = menuArray.map(el => {return {...el}})
 orderList[0].name='fgg'
 
-//renderMenu(menuArray)
- 
+//let clonedArray = JSON.parse(JSON.stringify(nodesArray))
+
+
+renderMenu(menuArray)
+renderOrder(menuArray)
+renderTotalPrice(menuArray)
+
+
 function renderMenu(dataToRender){
     const menuList = document.getElementById('menu-list');
 
@@ -33,10 +39,34 @@ function renderMenu(dataToRender){
 
 }
 
-function renderOrder(orderList){
+function renderOrder(orders){
+    const summaryList = document.getElementById('summary-list')
 
+    orders.forEach(element => {
+        const orderItem = document.createElement('li')
+        orderItem.classList.add('summary-item')
+        orderItem.innerHTML = `<h5>${element.name}</h5>`
+        const removeBtn = document.createElement('button')
+        removeBtn.textContent = 'remove'
+        removeBtn.addEventListener('click',function(){
+            console.log("tu bede usowac "+element.name)
+        })
+        orderItem.append(removeBtn)
+        const divPriceElement = document.createElement('div')
+        divPriceElement.classList.add('summary-item-price')
+        divPriceElement.textContent = '$'+element.price
+        orderItem.append(divPriceElement)
+        summaryList.append(orderItem)
+    })
 
 }
 
+function renderTotalPrice(orders){
+    let sum = 0;
+    orders.forEach(order => sum+= order.price)
+    document.getElementById('summary-total-price').textContent = '$ '+sum
+}
+
 console.log(orderList)
-console.log(menuArray)
+console.log()
+
